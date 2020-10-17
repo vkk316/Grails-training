fd88ec2dd8ad4713b2c49576ea85b108e8e58c97
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2020-10-17 22:29:02 +0700

    basic get - show all data

--- grails-app/controllers/box/BookController.groovy
+++ grails-app/controllers/box/BookController.groovy
@@ -8,13 +8,18 @@ class BookController {
 	static responseFormats = ['json']
 	
     def index() { 
-        def result = [valid: false]
-        def books = Book.list()
+        //Groovy เป็น lang ที่เป็น dynamic datatype
 
-        def totalcount = Book.count()
-        result.data = books
-        result.count = totalcount
+        // [ key: value ] <--- เรียกว่า Map คล้ายๆ Hash Table ใน C
+        def result = [valid: false]  // เป็น convention ของ Bonmek
+        def books = Book.list() // method list :: docs --> http://docs.grails.org/3.1.1/ref/Domain%20Classes/list.html 
+
+        def totalcount = Book.count() // Returns the total number of rows in the domain class table
+        // ที่ เอาค่าต่างๆ มาแมบ
+        result.data = books // เอา books ไปใส่ใน value ของ key "data" 
+        result.count = totalcount // เอา books ไปใส่ใน value ของ key "count" 
         result.valid = true
-        render(result as JSON)
+
+        render(result as JSON) // เอาท์พุต result เป็น json ที่ฝั่ง clien
     }
 }
